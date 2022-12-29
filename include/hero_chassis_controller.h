171e4eea4a9af45703d8b6349cc4d71580e343fa
Kook <kook@example.com>
2022-12-30 00:03:50 +0800

    update cpp file

--- include/hero_chassis_controller.h
+++ include/hero_chassis_controller.h
@@ -4,5 +4,68 @@
 
 #ifndef HERO_CHASSIS_CONTROLLER_HERO_CHASSIS_CONTROLLER_H
 #define HERO_CHASSIS_CONTROLLER_HERO_CHASSIS_CONTROLLER_H
+#include <controller_interface/controller.h>
+#include <hardware_interface/joint_command_interface.h> //Allow us to get and use joints ' infomation
+#include <control_toolbox/pid.h>
+#include <control_msgs/JointControllerState.h>
+#include <realtime_tools/realtime_publisher.h>
+#include <geometry_msgs/Twist.h>
+#include <ros/ros.h>
 
+namespace hero_chassis_controller {
+
+    class HeroChassisController : public controller_interface::Controller<hardware_interface::EffortJointInterface> {
+        //JointCommandInterface for commanding effort-based joints
+    public:
+        HeroChassisController() = default;
+        ~HeroChassisController() override ;
+        // The meaning of override is that I will write its codes again
+
+        bool init(hardware_interface::EffortJointInterface *effort_joint_interface,
+                  ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) override;
+        //This step is to load the connected joint and get the Param from the Param server
+
+        void update(const ros::Time &time, const ros::Duration &period) override;
+        //This meaning of it may be equal to while(1){}
+
+        hardware_interface::JointHandle front_left_joint_, front_right_joint_, back_left_joint_, back_right_joint_;
+
+
+
+    private:
+        //Nodehandle Pattern
+        int loop_count_;
+        std::unique_ptr<realtime_tools::RealtimePublisher<
+                control_msgs::JointControllerState> > controller_state_publisher_ ;
+        //Internal PID controller
+        control_toolbox::Pid pid_controller_1,pid_controller_2,pid_controller_3,pid_controller_4;
+
+        ros::Subscriber sub_command_;
+        //callback function for subscriber
+        void return_state(const geometry_msgs::TwistConstPtr &state_msg);
+
+
+
+        //Mecanum wheels pattern
+        //expected velocity of the chassis(through chassis to calculate the motor)
+        double Vx,Vy,Vw;
+        //actual velocity of wheels
+        double vel_wheel_act[5];
+        //expected vocity of four wheels
+        double vel_wheel_exp[5];
+        //comand velocity of four wheels through effort command
+        double wheel_cmd[5];
+        double Wheel_track;
+        double Wheel_base;
+        double Wheel_R = 0.07625;
+        //through chassis to calculate the motor
+        void compute_mecanum_vel();
+
+        //Time Pattern
+        int state_{};
+        ros::Time last_change_;
+
+    };
+
+}//namespace hero_chassis_controller
 #endif //HERO_CHASSIS_CONTROLLER_HERO_CHASSIS_CONTROLLER_H
--- src/hero_chassis_controller.cpp
+++ src/hero_chassis_controller.cpp
@@ -3,3 +3,124 @@
 //
 
 #include "hero_chassis_controller.h"
+#include <pluginlib/class_list_macros.hpp>
+
+namespace hero_chassis_controller{
+
+    HeroChassisController::~HeroChassisController(){
+        sub_command_.shutdown();
+    }
+
+    bool HeroChassisController::init(hardware_interface::EffortJointInterface *effort_joint_interface,
+                                     ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) {
+
+
+        //get joint information and handle from hardware_interface
+        front_left_joint_ =
+                effort_joint_interface->getHandle("left_front_wheel_joint");
+        front_right_joint_ =
+                effort_joint_interface->getHandle("right_front_wheel_joint");
+        back_left_joint_ =
+                effort_joint_interface->getHandle("left_back_wheel_joint");
+        back_right_joint_ =
+                effort_joint_interface->getHandle("right_back_wheel_joint");
+
+        //load PID controllers
+        pid_controller_1.init(controller_nh,"pid_1");
+        pid_controller_2.init(controller_nh,"pid_2");
+        pid_controller_3.init(controller_nh,"pid_3");
+        pid_controller_4.init(controller_nh,"pid_4");
+
+        //Initialiaze velocitizes
+        Vx=0.0;
+        Vy=0.0;
+        Vw=0.0;
+
+        // Start realtime state publisher
+        // warning: use std::make_unique
+        controller_state_publisher_ = std::make_unique<realtime_tools::RealtimePublisher
+                                                       <control_msgs::JointControllerState>>(root_nh, "state", 1);
+
+        //initialize the subscriber
+        sub_command_ =
+                root_nh.subscribe<geometry_msgs::Twist>
+                        ("cmd_vel", 1 , &HeroChassisController::return_state,this);
+
+        return true;
+
+    }
+
+    void HeroChassisController::update(const ros::Time &time, const ros::Duration &period) {
+
+
+        //get realtime velocity
+        vel_wheel_act[1] = front_left_joint_.getVelocity();
+        vel_wheel_act[2] = back_left_joint_.getVelocity();
+        vel_wheel_act[3] = back_right_joint_.getVelocity();
+        vel_wheel_act[4] = front_right_joint_.getVelocity();
+
+        //calculate velocity of the mecanum wheel
+        //Through your expected vx vy vw , which is the velocity of the chassis
+        compute_mecanum_vel();
+        //calculate error
+        double error[5];
+        for ( int i = 1 ; i < 5 ; i++ ) {
+            error[i] = vel_wheel_exp[i] - vel_wheel_act[i];
+        }
+        //compute and set effort command by PID controller
+        wheel_cmd[1] = pid_controller_1.computeCommand(error[1],period);
+        wheel_cmd[2] = pid_controller_2.computeCommand(error[2],period);
+        wheel_cmd[3] = pid_controller_3.computeCommand(error[3],period);
+        wheel_cmd[4] = pid_controller_4.computeCommand(error[4],period);
+
+
+
+        front_left_joint_.setCommand(wheel_cmd[1]);
+        back_left_joint_.setCommand(wheel_cmd[2]);
+        back_right_joint_.setCommand(wheel_cmd[3]);
+        front_right_joint_.setCommand(wheel_cmd[4]);
+
+
+        if(loop_count_ % 10 == 0)
+        {
+            if(controller_state_publisher_ && controller_state_publisher_->trylock())
+            {
+                controller_state_publisher_->msg_.header.stamp = time;
+                controller_state_publisher_->msg_.set_point = vel_wheel_exp[1];
+                controller_state_publisher_->msg_.process_value = front_left_joint_.getVelocity();
+                controller_state_publisher_->msg_.error = error[1];
+                controller_state_publisher_->msg_.time_step = period.toSec();
+                controller_state_publisher_->msg_.command = wheel_cmd[1];
+
+                double dummy;
+                bool antiwindup;
+                pid_controller_1.getGains(controller_state_publisher_->msg_.p,
+                                          controller_state_publisher_->msg_.i,
+                                          controller_state_publisher_->msg_.d,
+                                          controller_state_publisher_->msg_.i_clamp,
+                                          dummy,
+                                          antiwindup);
+                controller_state_publisher_->msg_.antiwindup = static_cast<char>(antiwindup);
+                controller_state_publisher_->unlockAndPublish();
+            }
+        }
+        loop_count_++;
+    }
+    void HeroChassisController::return_state(const geometry_msgs::TwistConstPtr &state_msg) {
+        Vx = state_msg->linear.x;
+        Vy = state_msg->linear.y;
+        Vw = state_msg->angular.z;
+
+    }
+    void HeroChassisController::compute_mecanum_vel() {
+        //wheels's order is anti-clockwise
+        vel_wheel_exp[1] = ( Vx - Vy - Vw * ( Wheel_base + Wheel_track )/2 )/ Wheel_R;
+        vel_wheel_exp[2] = ( Vx + Vy - Vw * ( Wheel_base + Wheel_track )/2 )/ Wheel_R;
+        vel_wheel_exp[3] = ( Vx - Vy + Vw * ( Wheel_base + Wheel_track )/2 )/ Wheel_R;
+        vel_wheel_exp[4] = ( Vx + Vy + Vw * ( Wheel_base + Wheel_track )/2 )/ Wheel_R;
+    }
+
+
+}//namespace
+
+PLUGINLIB_EXPORT_CLASS( hero_chassis_controller::HeroChassisController,controller_interface::ControllerBase)
